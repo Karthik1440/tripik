686716b846d3a555f5d2a131fbda908ca449dbe7
Karthik1440 <karthi809367@gmail.com>
2026-04-30 19:26:12 +0530

    add admin seed data

--- backend/core/trips/apps.py
+++ backend/core/trips/apps.py
@@ -1,11 +1,16 @@
 from django.apps import AppConfig
-from django.core.management import call_command
-
-
+from django.contrib.auth import get_user_model
 
 class TripsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trips'
 
-def ready(self):
-    call_command('loaddata', 'users.json')
+    def ready(self):
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
