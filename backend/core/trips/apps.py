c38a60bded8be764a7b56fa9cb44696dbc58ef04
Karthik1440 <karthi809367@gmail.com>
2026-04-30 19:17:30 +0530

    add admin seed data

--- backend/core/trips/apps.py
+++ backend/core/trips/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.core.management import call_command
+
 
 
 class TripsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trips'
+
+def ready(self):
+    call_command('loaddata', 'users.json')
