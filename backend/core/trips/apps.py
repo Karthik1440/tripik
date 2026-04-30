a3870d0dca9015081775c066e0d2405bdd5529df
Karthik1440 <karthi809367@gmail.com>
2026-04-30 20:05:11 +0530

    db chnaged

--- backend/core/trips/apps.py
+++ backend/core/trips/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
+
 
 class TripsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trips'
-
-    def ready(self):
-        User = get_user_model()
-
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@gmail.com",
-                password="admin123"
-            )
